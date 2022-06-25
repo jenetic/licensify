889fe6e72c724dcad875907f7f776509015c2a54
srnq <77878502+xzjenny@users.noreply.github.com>
2022-06-25 10:46:44 -0700

    Set up part of Spotify OAuth flow

---
+++ index.js
@@ -0,0 +1,81 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const port = 8888;
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+// Generate random string containing letters and numbers
+const generateRandomString = length => {
+  let text = '';
+  const possible = 
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+const stateKey = 'spotify_auth_state';
+
+
+app.get('/login', (req, res) => {
+  // state
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // scope
+  const scope = 'user-read-private user-read-email';
+
+  const searchParams = new URLSearchParams({
+    client_id: CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope,
+  });
+  const queryParams = searchParams.toString();
+
+  res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
+});
+
+app.get('/callback', (req, res) => {
+  // Get code from query, set to null if not found
+  const code = req.query.code || null;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: new URLSearchParams({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: REDIRECT_URI
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+    .then(response => {
+      if (response.status === 200) {
+        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+      } else {
+        res.send(response);
+      }
+    })
+    .catch(error => {
+      res.send(error);
+    });
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
