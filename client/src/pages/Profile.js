ad14241d8dc596a1a51cbc0ab32cda64c3e6624a
srnq <77878502+xzjenny@users.noreply.github.com>
2022-06-29 23:02:59 -0700

    Display top artist on profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -27,6 +27,8 @@ const Profile = () => {
         <div>
           <h1>{profile.display_name}</h1>
           <p>{profile.followers.total} Followers</p>
+          <p>{profile.id}</p>
+          <p>{topArtists && topArtists.items[0].name}</p>
           {profile.images.length && profile.images[0].url && (
             <img src={profile.images[0].url} alt="Profile Picture" />
           )}
