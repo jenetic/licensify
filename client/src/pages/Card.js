72bb3d8d2363a4a4e824a7b7357e1ef7a7d542c3
jenetic <jznnylam@gmail.com>
2022-07-15 19:43:20 -0700

    Clean up code

---
+++ client/src/pages/Card.js
@@ -0,0 +1,101 @@
+import { 
+  getCurrentDate, 
+  getLastName, 
+  getFirstName,
+} from '../utils';
+import noProfilePic from '../img/noProfilePic.jpg';
+
+const Card = ({
+    profile,
+    topArtist,
+    topTrackName,
+    topTrackArtists,
+    topTrackAlbumCover,
+    topGenres
+  }) => {
+  return (
+    <div id="card">
+      <div id="cardRectangle"></div>
+      <div id="loadingRectangle">
+        <p>loading...</p>
+      </div>
+      <div id="backgroundCoverArt" style={{backgroundImage: `url(${topTrackAlbumCover})`}}></div>
+      
+      <div id="mainProfilePicAndSignature">
+        <div id="mainProfilePicCrop">
+          {(profile.images.length && profile.images[0].url) ? (
+            <img src={profile.images[0].url} alt="large profile pic"></img>
+          ) : (
+            <img src={noProfilePic} alt="large default profile pic"></img>
+          )}
+        </div>
+        <p id="signature">{profile.display_name}</p>
+      </div>
+      
+      <p id="licensify">Licensify</p>
+      <p id="country">{profile.country}</p>
+      <p id="driverLicense">DRIVER LICENSE</p>
+      <p id="displayTimeRange">FROM LAST 6 MONTHS</p>
+
+      <hr id="topGreenLine"/>
+      <hr id="bottomGreenLine"/>
+
+      <div id="topSection" className="flexWrapper">
+        <div id="dl" className="flexWrapper">
+          <p id="dlKey" className="sectionKey">DL</p>
+          <p id="dlValue">{profile.id.toUpperCase()}</p>
+        </div>
+        
+        <div id="expAndFollowersWrapper" className="flexWrapper">
+          <p id="expKey" className="sectionKey">EXP</p>
+          <p id="expValue">{getCurrentDate()}</p>
+
+          <p id="followersKey" className="sectionKey">FOLLOWERS</p>
+          <p id="followersValue" className="sectionValue">{profile.followers.total}</p>
+        </div>
+
+        <div id="nameWrapper" className="flexWrapper">
+          <div id="ln" className="flexWrapper">
+            <p id="lnKey" className="sectionKey">LN</p>
+            <p id="lnValue" className="sectionValue">{getLastName(profile.display_name).toUpperCase()}</p>
+          </div>
+
+          <div id="fn" className="flexWrapper">
+            <p id="fnKey" className="sectionKey">FN</p>
+            <p id="fnValue" className="sectionValue">{getFirstName(profile.display_name).toUpperCase()}</p>
+          </div>
+        </div>
+
+        <div id="trackAndArtistWrapper" className="flexWrapper">
+          <div id="track" className="flexWrapper">
+            <p id="trackKey" className="sectionKey">#1 TRACK</p>
+            <p id="trackValue" className="sectionValue">{topTrackArtists} - {topTrackName}</p>
+          </div>
+          <div id="artist" className="flexWrapper"> 
+            <p id="artistKey" className="sectionKey">#1 ARTIST</p>
+            <p id="artistValue" className="sectionValue">{topArtist.toUpperCase()}</p>
+          </div>
+        </div>
+      </div>
+    
+      <div id="genres" className="flexWrapper">
+        <p id="genresKey" className="sectionKey">TOP GENRES</p>
+        <p id="genresValue" className="sectionValue">
+          <span>{topGenres[0]},      </span>    
+          <span>{topGenres[1]},      </span>
+          <span>{topGenres[2]}</span>
+        </p>
+      </div>
+
+      {(profile.images.length && profile.images[0].url) ? (
+        <img id="smallProfilePic" src={profile.images[0].url} alt="small profile pic"></img>
+      ) : (
+        <img id="smallProfilePic" src={noProfilePic} alt="small default profile pic"></img>
+      )}
+
+      <p id="websiteName">LICENSIFY.HEROKUAPP.COM</p>
+    </div>
+  )
+}
+
+export default Card;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { 
-  // accessToken,
   getAccessToken,
   getCurrentUserProfile,
   getTopArtists,
@@ -11,12 +10,8 @@ import {
 } from '../spotify';
 import { 
   catchErrors,
-  getCurrentDate, 
-  getLastName, 
-  getFirstName,
 } from '../utils';
-import noProfilePic from '../img/noProfilePic.jpg';
-
+import Card from './Card';
 
 const Profile = () => {
 
@@ -27,27 +22,38 @@ const Profile = () => {
   const [topTrackAlbumCover, setTopTrackAlbumCover] = useState("");
   const [topGenres, setTopGenres] = useState("");
 
-  // Changes Spotify data to fit specified time range
-  const changeTimeRange = async (time_range) => {
+  const displayCardContents = async (time_range="medium_term", refreshStatus) => {
 
-    const accessToken = await getAccessToken();
-
-    document.getElementById("loadingRectangle").style.display = "flex";
+    // Prevent error of loadingRectangle not having loaded in yet when refreshed
+    if (!refreshStatus) {
+      document.getElementById("loadingRectangle").style.display = "flex";
+    }
 
     const userTopArtists = await getTopArtists(time_range);
     setTopArtist(userTopArtists.data.items[0].name);
-
+  
     const userTopTrackName = await getTopTrackName(time_range);
     setTopTrackName(userTopTrackName);
-
+  
     const userTopTrackArtists = await getTopTrackArtists(time_range);
     setTopTrackArtists(userTopTrackArtists);
-
+  
     const userTopTrackAlbumCover = await getTopTrackAlbumCover(time_range);
     setTopTrackAlbumCover(userTopTrackAlbumCover);
-
+  
     const userTopGenres = await getTopGenres(time_range);
     setTopGenres(userTopGenres);
+
+    document.getElementById("loadingRectangle").style.display = "none";
+  }
+
+  // Changes Spotify data to fit specified time range
+  const changeTimeRange = async (time_range) => {
+
+    // TODO: Implement better way of refreshing token after token expires
+    const accessToken = await getAccessToken();
+
+    displayCardContents(time_range, false);
 
     // Display time range
     if (time_range === "short_term") {
@@ -57,34 +63,27 @@ const Profile = () => {
     } else {
       document.getElementById("displayTimeRange").textContent = "FROM LAST 6 MONTHS";
     }
-
-    document.getElementById("loadingRectangle").style.display = "none";
   }
 
+  // Get user profile data
   useEffect(() => {
-    // Get user profile data
     const fetchUserProfileData = async () => {
-
       const userProfile = await getCurrentUserProfile();
       setProfile(userProfile.data);
-      
-      const userTopArtists = await getTopArtists();
-      setTopArtist(userTopArtists.data.items[0].name);
 
-      const userTopTrackName = await getTopTrackName();
-      setTopTrackName(userTopTrackName);
-
-      const userTopTrackArtists = await getTopTrackArtists();
-      setTopTrackArtists(userTopTrackArtists);
-
-      const userTopTrackAlbumCover = await getTopTrackAlbumCover();
-      setTopTrackAlbumCover(userTopTrackAlbumCover);
-
-      const userTopGenres = await getTopGenres();
-      setTopGenres(userTopGenres);
+      displayCardContents("medium_term", true);
     }
     catchErrors(fetchUserProfileData());
   }, [])
+
+  let props = {
+    profile,
+    topArtist,
+    topTrackName,
+    topTrackArtists,
+    topTrackAlbumCover,
+    topGenres
+  }
 
   return (
     <>
@@ -92,87 +91,7 @@ const Profile = () => {
         <div id="profilePage">
           <p id="loading"></p>
           <div id="cardWrapper">
-            <div id="card">
-              <div id="cardRectangle"></div>
-              <div id="loadingRectangle">
-                <p>loading...</p>
-              </div>
-              <div id="backgroundCoverArt" style={{backgroundImage: `url(${topTrackAlbumCover})`}}></div>
-              
-              <div id="mainProfilePicAndSignature">
-                <div id="mainProfilePicCrop">
-                  {(profile.images.length && profile.images[0].url) ? (
-                    <img src={profile.images[0].url} alt="large profile pic"></img>
-                  ) : (
-                    <img src={noProfilePic} alt="large default profile pic"></img>
-                  )}
-                </div>
-                <p id="signature">{profile.display_name}</p>
-              </div>
-              
-              <p id="licensify">Licensify</p>
-              <p id="country">{profile.country}</p>
-              <p id="driverLicense">DRIVER LICENSE</p>
-              <p id="displayTimeRange">FROM LAST 6 MONTHS</p>
-
-              <hr id="topGreenLine"/>
-              <hr id="bottomGreenLine"/>
-
-              <div id="topSection" className="flexWrapper">
-                <div id="dl" className="flexWrapper">
-                  <p id="dlKey" className="sectionKey">DL</p>
-                  <p id="dlValue">{profile.id.toUpperCase()}</p>
-                </div>
-                
-                <div id="expAndFollowersWrapper" className="flexWrapper">
-                  <p id="expKey" className="sectionKey">EXP</p>
-                  <p id="expValue">{getCurrentDate()}</p>
-
-                  <p id="followersKey" className="sectionKey">FOLLOWERS</p>
-                  <p id="followersValue" className="sectionValue">{profile.followers.total}</p>
-                </div>
-
-                <div id="nameWrapper" className="flexWrapper">
-                  <div id="ln" className="flexWrapper">
-                    <p id="lnKey" className="sectionKey">LN</p>
-                    <p id="lnValue" className="sectionValue">{getLastName(profile.display_name).toUpperCase()}</p>
-                  </div>
-
-                  <div id="fn" className="flexWrapper">
-                    <p id="fnKey" className="sectionKey">FN</p>
-                    <p id="fnValue" className="sectionValue">{getFirstName(profile.display_name).toUpperCase()}</p>
-                  </div>
-                </div>
-
-                <div id="trackAndArtistWrapper" className="flexWrapper">
-                  <div id="track" className="flexWrapper">
-                    <p id="trackKey" className="sectionKey">#1 TRACK</p>
-                    <p id="trackValue" className="sectionValue">{topTrackArtists} - {topTrackName}</p>
-                  </div>
-                  <div id="artist" className="flexWrapper"> 
-                    <p id="artistKey" className="sectionKey">#1 ARTIST</p>
-                    <p id="artistValue" className="sectionValue">{topArtist.toUpperCase()}</p>
-                  </div>
-                </div>
-              </div>
-            
-              <div id="genres" className="flexWrapper">
-                <p id="genresKey" className="sectionKey">TOP GENRES</p>
-                <p id="genresValue" className="sectionValue">
-                  <span>{topGenres[0]},      </span>    
-                  <span>{topGenres[1]},      </span>
-                  <span>{topGenres[2]}</span>
-                </p>
-              </div>
-
-              {(profile.images.length && profile.images[0].url) ? (
-                <img id="smallProfilePic" src={profile.images[0].url} alt="small profile pic"></img>
-              ) : (
-                <img id="smallProfilePic" src={noProfilePic} alt="small default profile pic"></img>
-              )}
-
-              <p id="websiteName">LICENSIFY.HEROKUAPP.COM</p>
-            </div>
+            <Card {...props}/>
           </div>
 
             <div id="timeRangeButtons">
