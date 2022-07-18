7a096cff2a00824dff20a7398791a511eda5fa22
jenetic <jznnylam@gmail.com>
2022-07-18 09:39:07 -0700

    Update about page with privacy policy

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -10,11 +10,11 @@ const About = () => {
           in the future, but I haven't thought that far yet.
         </p>
         <p>
-          Licensify was made with React.js, Node.js, and Express.js. I used React.js and CSS to create the driver license itself and the
+          Licensify is was made with React.js, Node.js, and Express.js. I used React.js and CSS to create the driver license itself and the
           Spotify Web API to retrieve user listening activity from Spotify, as well as authorize Spotify accounts.
         </p>
         <p>
-          View the Github repo <a href="https://github.com/srnq/licensify" target="_blank" rel="noreferrer">here</a>.
+          Licensify is open source; view the Github repo <a href="https://github.com/srnq/licensify" target="_blank" rel="noreferrer">here</a>.
         </p>
       </div>
 
@@ -26,8 +26,7 @@ const About = () => {
         </p>
         <p id="contactLinks">
           <a href="https://jenetic.github.io" target="_blank" rel="noreferrer">website</a> |
-          <a href="https://github.com/jenetic" target="_blank" rel="noreferrer">github</a> | 
-          <a href="https://www.linkedin.com/in/jwny/" target="_blank" rel="noreferrer">linkedin</a>
+          <a href="https://github.com/jenetic" target="_blank" rel="noreferrer">github</a>
         </p>
       </div>
 
@@ -39,6 +38,17 @@ const About = () => {
         </p>
         <p>
           If you find other issues or have any suggestions, you can open an issue in the Github repo or contact me any other way.
+        </p>
+      </div>
+
+      <div className="qna">
+        <h2>privacy policy</h2>
+        <p>
+          None of the data used by Licensify is stored, collected, or shared with any third parties.
+          All information is used only for generating your driver license. I do not care about your listening activity or Spotify account that much to store your data.
+        </p>
+        <p>
+          Remove Licensify's access to Spotify <a href="https://support.spotify.com/us/article/spotify-on-other-apps/" target="_blank" rel="noreferrer">here</a>.
         </p>
       </div>
     </div>
