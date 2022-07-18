ca833169e51f25e921d84774ea94cd822fb4b5f6
jenetic <jznnylam@gmail.com>
2022-07-18 09:45:39 -0700

    Uninstall react-ga

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,6 @@ import {
 } from 'react-router-dom';
 import './css/Card.css'; 
 import './css/Main.css';
-import ReactGA from 'react-ga';
-
-const TRACKING_ID = "G-ZD9Z6C3XWD";
-ReactGA.initialize(TRACKING_ID);
 
 function App() {
 
